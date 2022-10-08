5d036714340ab6e876b6550c302787a8763398f2
githubYubella <anmpbella@gmail.com>
2022-10-08 10:09:21 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas_akhir_2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
